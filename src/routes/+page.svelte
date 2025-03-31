559d6b4fb1a066948b234dc77ee6723563a785f1
Deez <deeznuras@do-not-airdrip-4.local>
2025-03-31 14:42:47 +0800

    feat: add responsiveness

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
 <h1>Rush Hour</h1>
+<style>
+@media (max-width: 500px) {
+    body {
+      background-color: #ffffff
+    }
+  }
+  @media (max-width: 700px) {
+    body {
+      background-color: #ffffff
+    }
+  }
+  
+  .red {
+    color: red;
+  }
+  </style>
